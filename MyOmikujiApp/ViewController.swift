5e0e103834035706866742e31710ef56f2017554
shgtkshruch <shgtk.shruch@gmail.com>
2016-06-29 18:21:05 +0900

    Initial Commit

---
+++ MyOmikujiApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyOmikujiApp
+//
+//  Created by shgtkshruch on 2016/06/29.
+//  Copyright © 2016年 shgtkshruch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
